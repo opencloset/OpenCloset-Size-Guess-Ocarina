52b8024652c31a8563b241425831d9565a6309ed
Yongbin Yu <yongbin.yu@gmail.com>
2016-01-03 15:11:41 +0900

    Implement OpenCloset::Size::Guess::Ocarina driver
    REST API를 호출해서 결과를 제공하는 방식이 유사하기 때문에 대부분의
    코드를 OpenCloset::Size::Guess::BodyKit을 참고해서 구현했습니다.
    참고할만한 차이점은 아래와 같습니다.
    
      - BodyKit의 경우 REST API 인증부가 있지만 Ocarina의 경우 별도의
        인증부가 없습니다.
      - BodyKit의 경우 단일 호출을 통해 결과 JSON을 가지고 오지만 Ocarina의
        경우 첫번째 요청으로 결과를 담고있는 URL을 전달받은 뒤 해당 URL을
        다시한번 호출해서 값을 가지고 오는 구조입니다.

--- lib/OpenCloset/Size/Guess/Ocarina.pm
+++ lib/OpenCloset/Size/Guess/Ocarina.pm
@@ -1,9 +1,177 @@
 package OpenCloset::Size::Guess::Ocarina;
 # ABSTRACT: OpenCloset::Size::Guess driver for the Ocarina
 
-use strict;
-use warnings;
+use utf8;
+
+use Moo;
+use Types::Standard qw( Str Int );
 
 our $VERSION = '0.002';
 
+with 'OpenCloset::Size::Guess::Role::Base';
+
+use HTTP::Tiny;
+use JSON;
+use Try::Tiny;
+use List::MoreUtils qw/ all mesh /;
+
+#
+# to support HTTPS
+#
+
+use IO::Socket::SSL;
+use Mozilla::CA;
+use Net::SSLeay;
+
+#<<< skip perltidy
+has url         => ( is => 'ro', isa => Str, default => 'https://brompton.silex.kr//ocpu/tmp/ocarina/R/size' );
+has bust        => ( is => 'ro', isa => Int );
+has waist       => ( is => 'ro', isa => Int );
+has topbelly    => ( is => 'ro', isa => Int );
+has thigh       => ( is => 'ro', isa => Int );
+has arm         => ( is => 'ro', isa => Int );
+has leg         => ( is => 'ro', isa => Int );
+#>>>
+
+sub guess {
+    my $self = shift;
+
+    my %ret = (
+        height   => $self->height,
+        weight   => $self->weight,
+        gender   => $self->gender,
+        arm      => undef,
+        bust     => undef,
+        foot     => undef,
+        hip      => undef,
+        knee     => undef,
+        leg      => undef,
+        thigh    => undef,
+        topbelly => undef,
+        waist    => undef,
+        belly    => undef,
+        success  => 0,
+        reason   => q{},
+    );
+
+    my %params = (
+        g        => sprintf( q{'%s'}, $self->gender ),
+        height   => $self->height,
+        weight   => $self->weight,
+        bust     => $self->bust,
+        waist    => $self->waist,
+        topbelly => $self->topbelly,
+        thigh    => $self->thigh,
+        arm      => $self->arm,
+        leg      => $self->leg,
+    );
+
+    my $http = HTTP::Tiny->new();
+    my $guess = $http->post_form( $self->url, \%params );
+    unless ( $guess->{success} ) {
+        $ret{reason} = "$guess->{status}: $guess->{content}";
+        return \%ret;
+    }
+
+    my @ids;
+    for my $line ( split( /\n/, $guess->{content} ) ) {
+        my ($id) = $line =~ m{^/ocpu/tmp/(x[0-9a-f]{10})/.*$};
+        push @ids, $id;
+    }
+    my $id = shift @ids;
+    unless ( all { $_ eq $id } @ids ) {
+        $ret{reason} = "invalid server response : $guess->{congent}";
+        return \%ret;
+    }
+
+    my $value_url = "https://brompton.silex.kr/ocpu/tmp/${id}/R/.val/json";
+    my $res       = $http->get($value_url);
+    unless ( $res->{success} ) {
+        $ret{reason} = "$res->{status}: $res->{content}";
+        return \%ret;
+    }
+
+    my $data = try { JSON::decode_json( $res->{content} ) };
+    unless ($data) {
+        $ret{reason} = "failed to decode json string: $res->{content}";
+        return \%ret;
+    }
+
+    while ( my ( $k, $v ) = each %{$data} ) {
+        my @keys = keys %{$v};
+        my @values = map { $_->[0] } values %{$v};
+
+        %ret = ( %ret, mesh @keys, @values );
+    }
+
+    return \%ret;
+}
+
 1;
+
+# COPYRIGHT
+
+__END__
+
+=for Pod::Coverage BUILDARGS
+
+=head1 SYNOPSIS
+    use OpenCloset::Size::Guess;
+
+    my $guesser = OpenCloset::Size::Guess->new(
+        'Ocarina',
+        gender     => 'male',
+        height     => 183,
+        weight     => 82,
+        _bust      => 102,
+        _waist     => 88,
+        _topbelly  => 85,
+        _thigh     => 61,
+        _arm       => 64,
+        _leg       => 105,
+    );
+
+    my $result = $guesser->guess;
+
+    print "bust     : $result->{bust}\n";
+    print "topbelly : $result->{topbelly}\n";
+    print "arm      : $result->{arm}\n";
+    print "waist    : $result->{waist}\n";
+    print "thigh    : $result->{thigh}\n";
+
+
+=head1 DESCRIPTION
+
+This module is a L<OpenCloset::Size::Guess> driver for the Ocarina(OpenCPU based R Rest interface) service.
+
+
+=attr height
+
+=attr weight
+
+=attr gender
+
+=attr url
+
+=attr bust
+
+=attr waist
+
+=attr topbelly
+
+=attr thigh
+
+=attr arm
+
+=attr leg
+
+=method guess
+
+=head1 SEE ALSO
+
+=for :list
+* L<Search issue|https://github.com/opencloset/opencloset/issues/627>
+* L<Refactoring R - Ocarina|https://github.com/yongbin/refactoring-r>
+* L<OpenCloset::Size::Guess>
+* L<SMS::Send>
+* L<Parcel::Track>
