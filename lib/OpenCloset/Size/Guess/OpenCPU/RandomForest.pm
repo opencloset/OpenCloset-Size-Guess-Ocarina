53fe3ea5a79c7ca895befe345cdd82374b5da90a
Yongbin Yu <yongbin.yu@gmail.com>
2016-07-11 23:56:21 +0900

    Handle indeterminable args
    기존 인자에서 고정으로 할당하던 키의 값을 전달받은 값에 따라서 존재하는
    키만 인자에 추가하도록 변경함으로써 성별에 따라 전달받은 인자가
    다르더라도 적절하게 ocarina 호출이 가능합니다.

--- lib/OpenCloset/Size/Guess/OpenCPU/RandomForest.pm
+++ lib/OpenCloset/Size/Guess/OpenCPU/RandomForest.pm
@@ -46,14 +46,12 @@ sub guess {
         result   => q{'list'},
         height   => $self->height,
         weight   => $self->weight,
-        bust     => $self->bust,
-        waist    => $self->waist,
-        topbelly => $self->topbelly,
-        thigh    => $self->thigh,
-        hip      => $self->hip,
-        arm      => $self->arm,
-        leg      => $self->leg,
     );
+
+    for my $k (qw/bust waist topbelly thigh hip arm leg/) {
+        next unless $self->$k;
+        $params{$k} = $self->$k;
+    }
 
     my $http = HTTP::Tiny->new();
     my $guess = $http->post_form( $self->url, \%params );
